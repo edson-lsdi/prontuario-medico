5adefa61a1bdbf52051a490fc1e90c8367d32462
edson.sampaio <edson.sampaio@lsdi.ufma.br>
2025-02-28 11:03:44 -0300

    add server

---
+++ application/server/src/main/java/com/lsdi/prontuario_medico/ProntuarioMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.lsdi.prontuario_medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuarioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuarioMedicoApplication.class, args);
+	}
+
+}
---
+++ application/server/src/test/java/com/lsdi/prontuario_medico/ProntuarioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsdi.prontuario_medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntuarioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
